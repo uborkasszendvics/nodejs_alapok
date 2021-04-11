72323215c446715c80a26c2e2372027e5127b508
Istenes Imre <imre.istenes@q-net.com>
2021-04-11 10:15:37 +0200

    kezdő commit

---
+++ fügvények_metódusok.js
@@ -0,0 +1,45 @@
+// Egysoros komment
+
+/*
+Több soros komment
+bla bla bla
+bla*/
+
+/**
+ * visszatér a bemenő érték négyzetével
+ * @param szám
+ * @return szám négyzete
+ */
+function negyzet(x) {
+    return x * x;
+}
+
+
+function terulet(a, b) {
+    return a * b;
+}
+
+
+function main() {
+    let result = terulet(5, 4);
+    console.log(result);
+}
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
